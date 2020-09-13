bd856d6ed968cfd892637662b3c4dde7058eead9
Teho <changteho11@gmail.com>
2020-09-13 17:46:31 +0800

    now I got .gitignore

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,11 +1,12 @@
   
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path=require('path')
+const webpack=require('webpack')
    
 
   module.exports={
       mode:'development',
-      entry:'./static/JS/index.js',
+      entry:'./JS/index.js',
       devServer:{
           contentBase:'./dist'
       },
@@ -20,12 +21,29 @@ const path=require('path')
                 presets: ['@babel/preset-env']
               }
             }
+          },
+          { test: /\.css$/, 
+            use: [ 'style-loader', 'css-loader' ]
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use:[
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ]
           }
         ]
       },
       plugins:[
         new HtmlWebpackPlugin({
             title:'Development',
+        }),
+        new HtmlWebpackPlugin({
+          template: './public/index.html'
         })
       ],
 
