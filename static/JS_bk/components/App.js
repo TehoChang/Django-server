f898a05001e3761f0333696a7825e998ae4b9d18
jsutin <justin@>
2020-09-15 07:44:11 +0800

    add teho backup

---
+++ static/JS_bk/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from 'antd'
+import Home from './Home'
+// import NavBar from './NavBar';
+
+class App extends React.Component{
+    render(){
+       return ( 
+        <Home/>
+     )
+    }
+}
+
+export default App;
